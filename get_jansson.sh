21606912bce154afaf5c2fecf8b1e78837ec7bee
Darau, Blė <darau.ble@gmail.com>
2020-02-07 07:25:37 +0000

    Skriptai biblioteku parsiuntimui

---
+++ get_jansson.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget http://www.digip.org/jansson/releases/jansson-2.12.tar.gz
+tar -xzf jansson-2.12.tar.gz
+ln -s jansson-2.12 jansson
---
+++ get_mqtt.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/eclipse/paho.mqtt.c
+cd paho.mqtt.c && git checkout v1.3.1
+
